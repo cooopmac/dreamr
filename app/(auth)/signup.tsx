af17924972b802fc9124ae3df04703bae9e4ba14
cooopmac <cooper.macgregor14@gmail.com>
2025-06-17 23:13:45 -0400

    file structure setup

---
+++ app/(auth)/signup.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Signup() {
+    return (
+        <SafeAreaView>
+            <Text>Signup</Text>
+        </SafeAreaView>
+    );
+}
